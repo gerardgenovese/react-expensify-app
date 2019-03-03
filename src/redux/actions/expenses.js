902ff9b68edab6b70c4eae4731cb88bc57e43126
Gerard Genovese <gmoneymaster24@yahoo.com>
2019-03-02 20:43:06 -0500

    Setup test database environment

--- src/redux/actions/expenses.js
+++ src/redux/actions/expenses.js
@@ -1,23 +1,64 @@
 import uuid from "uuid";
+import database from "../../firebase/firebase";
 
-//add expense
-const addExpense = (
-	{
-		description = "",
-		note = "",
-		amount = 0,
-		createdAt = 0
-	} = {}
-) => ({
+//regualar
+//component calls action generator
+//action generator returns object
+//component dispatches object
+//redux store changes
+
+
+//async
+//component calls action generator
+//action generator returns function
+//component dispatches function
+//function runs (has the abiity to dispatch other actions and do whatever it wants)
+
+//add expense dispatches object
+//before redux thunk
+// const addExpense = (
+// 	{
+// 		description = "",
+// 		note = "",
+// 		amount = 0,
+// 		createdAt = 0
+// 	} = {}
+// ) => ({
+// 	type: "ADD_EXPENSE",
+// 	expense: {
+// 		id: uuid(),
+// 		description,
+// 		note,
+// 		amount,
+// 		createdAt
+// 	}
+// });
+
+//after redux thunk
+const addExpense = (expense) => ({
 	type: "ADD_EXPENSE",
-	expense: {
-		id: uuid(),
-		description,
-		note,
-		amount,
-		createdAt
-	}
+	expense
 });
+
+//AFter redux thunk. Start the addexpense process which will keep changing store. Returning a function which is why we need redux thunk middleware. 
+const startAddExpense = (expenseData = {}) => {
+	return (dispatch) => {
+		const { 
+			description = "",
+			note = "",
+			amount = 0,
+			createdAt = 0
+		} = expenseData;
+		const expense = { description, note, amount, createdAt };
+		
+		return database.ref("expenses").push(expense).then((ref) => {
+			dispatch(addExpense( {
+				id: ref.key,
+				...expense
+			}))
+		});
+	};
+};
 
 //remove expense
 const removeExpense = ({ id } = {}) => ({
@@ -32,4 +73,4 @@ const editExpense = (id, updates) => ({
 	updates
 });
 
-export { addExpense, removeExpense, editExpense };
+export { addExpense, startAddExpense, removeExpense, editExpense };
--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,6 +1,29 @@
-import { createStore, combineReducers } from "redux";
+
+//before thunk
+// import { createStore, combineReducers } from "redux";
+// import expensesReducer from "../reducers/expenses";
+// import filtersReducer from "../reducers/filters";
+
+// export default () => {
+// 	//Store creation
+// 	const store = createStore(
+// 		combineReducers({
+// 			expenses: expensesReducer,
+// 			filters: filtersReducer
+// 		}),
+// 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// 	);
+// 	return store;
+// };
+
+
+//with thunk
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import expensesReducer from "../reducers/expenses";
 import filtersReducer from "../reducers/filters";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
 
 export default () => {
 	//Store creation
@@ -9,8 +32,7 @@ export default () => {
 			expenses: expensesReducer,
 			filters: filtersReducer
 		}),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		composeEnhancers(applyMiddleware(thunk))
 	);
 	return store;
 };
-
